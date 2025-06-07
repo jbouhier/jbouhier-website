4f28fd58a46d14ba039031dbcfc615a713c08503
JB Bouhier <jb.bouhier@gmail.com>
2025-06-07 00:22:24 -0700

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'static',
-  // For GitHub Pages
-  base: process.env.GITHUB_PAGES === 'true' 
-    ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1] || ''}`
-    : '/',
   site: 'https://jbouhier.com',
   integrations: [
     tailwind(), 
