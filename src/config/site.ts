4abc25deb57643c25a49facafe12ca7f8949a223
JB Bouhier <jb.bouhier@gmail.com>
2025-06-11 15:37:36 -0700

    copy

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,11 +1,11 @@
-export const siteConfig = {
-  title: "JB | Builds startups",
-  description: "I build and grow bootstrapped SaaS products. Follow my journey as I share insights and lessons learned.",
-  baseUrl: process.env.SITE_URL || 'https://jbouhier.com',
+export const site = {
+  title: "JB | Building startups",
+  description: "I build and grow startups. Follow my journey as I share insights and lessons learned.",
+  url: process.env.SITE_URL || 'https://jbouhier.com',
 } as const;
 
 export const author = {
   firstName: "JB",
   lastName: "Bouhier",
-  subtitle: "Building bootstrapped SaaS products",
+  subtitle: "Building startups",
 } as const;
